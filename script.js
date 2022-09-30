92405c8df143cec48bda6906ba62ce5f2ee0653f
Victor Apaez <vic.apaez@gmail.com>
2022-09-30 11:22:11 -0400

    movie slider with modals

--- script.js
+++ script.js
@@ -1,13 +1,68 @@
 let apiKey = "4bb0e757619267e381c73a006aa412e2";
-let url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`;
-fetch(url)
-  .then((res) => res.json())
-  .then((res) => {
-    console.log(res.results[0]);
-    let movieId = res.results[0].id;
-    let moviePath = res.results[0].poster_path;
-    let html = `<img src=https://image.tmdb.org/t/p/w185${moviePath}><img>`;
-    document.body.insertAdjacentHTML("afterbegin", html);
+let urlTopMovies = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`;
+let urlNowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
+let urlComingSoon = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`;
+
+function createUI(res, rowName) {
+  res.results.forEach((movieInfo) => {
+    let moviePath = movieInfo.poster_path;
+    let html = `
+      <div class="poster"
+        style="background-image: url(https://image.tmdb.org/t/p/w185${moviePath});">
+        <dialog class="modal ${movieInfo.id}">
+          <div class="modal_content">
+            <img src=https://image.tmdb.org/t/p/w185${moviePath} alt="">
+            <div>
+              <h1>${movieInfo.title}</h1>
+              <p>${movieInfo.overview}</p>
+          </div>
+        <div>
+      </dialog>
+      </div>
+      
+    `;
+    document.querySelector(rowName).insertAdjacentHTML("beforeend", html);
   });
-// https://image.tmdb.org/t/p/w185/
-// /3bhkrj58Vtu7enYsRolD1fZdja1.jpg
+}
+
+function clickButtons(element, num) {
+  const nextBtn = document.querySelectorAll(".btn-next")[num];
+  const prevBtn = document.querySelectorAll(".btn-prev")[num];
+  const width = document.querySelector(".top-rated").offsetWidth;
+  const posters = document.querySelectorAll(element);
+
+  posters.forEach((poster) => {
+    poster.addEventListener("click", (e) => {
+      e.target.querySelector(".modal").showModal();
+    });
+  });
+
+  nextBtn.addEventListener("click", (e) => {
+    e.path[1].querySelector(element).scrollBy({
+      top: 0,
+      left: width,
+      behavior: "smooth",
+    });
+  });
+
+  prevBtn.addEventListener("click", (e) => {
+    e.path[1].querySelector(element).scrollBy({
+      top: 0,
+      left: -width,
+      behavior: "smooth",
+    });
+  });
+}
+
+function fetchData(url, rowName, index) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      createUI(res, rowName);
+      clickButtons(rowName, index);
+    });
+}
+
+fetchData(urlTopMovies, ".top-rated", 0);
+fetchData(urlNowPlaying, ".now-playing", 1);
+fetchData(urlComingSoon, ".coming-soon", 2);
